d0ddfeef6c6d9992e0d382b4f9575f20cab63626
Artem Dembovskiy <DVL.45@mail.ru>
2023-12-18 01:42:51 +0300

    Added uvicorn command to main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi_profiler import PyInstrumentProfilerMiddleware
+import uvicorn
 
 from src.routers.routers import process_router
 from src.middlewares import ContentTypeMiddleware
 
 
-app = FastAPI(title='Process data service')
-app.add_middleware(PyInstrumentProfilerMiddleware)
-app.add_middleware(ContentTypeMiddleware)
+if __name__ == '__main__':
+    app = FastAPI(title='Process data service')
+    app.add_middleware(PyInstrumentProfilerMiddleware)
+    app.add_middleware(ContentTypeMiddleware)
 
-app.include_router(process_router, prefix="/api")
+    app.include_router(process_router, prefix="/api")
+    uvicorn.run(app, host='0.0.0.0', port=8000)
